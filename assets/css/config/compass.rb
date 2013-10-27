4af3027d43fbd805b5feca2adf6acc62112f7ed0
Luiz Gustavo Freire Gama <luizgamabh@gmail.com>
2013-10-27 03:51:24 -0200

    changes

---
+++ assets/css/config/compass.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "../css"
+javascripts_dir = "../js"
+images_dir = "../img"
+images_path = "../img"
+sprite_load_path = "../img"
+fonts_dir = "../fontes"
+
+output_style = :compressed
+relative_assets = true
+line_comments = false
+
+Sass::Script::Number.precision = 10
